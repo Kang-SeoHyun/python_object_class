20a5cd71de41a5f949a60f892d65421221333452
Kang-SeoHyun <kang061020@gmail.com>
2022-09-29 13:09:56 +0900

    modi:task05

--- task/05_2018112151_강서현_2.py
+++ task/05_2018112151_강서현_2.py
@@ -1,10 +1,10 @@
-print(input()[::-1])
+#print(input()[::-1])
 
-#word = input()
-#def is_palindrom(word):
-#    return word == word[::-1]
+word = input()
+def is_palindrom(word):
+    return word == word[::-1]
 
-#if is_palindrom(word) == True:
-#    print(word)
-#else:
-#    print(word[::-1])
+if is_palindrom(word) == True:
+    print(word)
+else:
+    print(word[::-1])
