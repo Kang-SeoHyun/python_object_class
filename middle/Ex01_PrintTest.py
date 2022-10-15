b61ae374a26e2c9fe6d0ed62a677bb8f52085852
Kang-SeoHyun <kang061020@gmail.com>
2022-10-15 23:51:20 +0900

    add: ~part6

--- middle/Ex01_PrintTest.py
+++ middle/Ex01_PrintTest.py
@@ -1,3 +1,9 @@
 f = -3
 print(type(f))
 print("hi " * 20)
+
+for i in range(3) :
+	for k in range(2) :
+		print("i는 %d고, k는 %d다." % (i, k) + '123', 123)
+		#이건 안됨.
+		# print("i는 %d고, k는 %d다." % (i, k), 123 + '123')
